8fc2c6cd96739c6973cd51ca33a656b41d4967e6
Ahmed Chikh Salah <ahmedchikhsalah4@gmail.com>
2026-05-05 22:39:51 +0100

    fix the problem of queue and hibernation

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -270,7 +270,7 @@ class AuthController extends Controller
             }
         } else {
             try {
-                Mail::to($user->email)->queue(new OtpMail($otpString));
+                Mail::to($user->email)->send(new OtpMail($otpString));
                 $channel = 'email';
             } catch (\Exception $e) {
                 \Log::error('OTP Email Error: ' . $e->getMessage());
