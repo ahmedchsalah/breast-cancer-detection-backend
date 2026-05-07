16bb6c5f38f6c8b37154a544c1a652c94ae72da6
Ahmed Chikh Salah <ahmedchikhsalah4@gmail.com>
2026-05-07 11:06:51 +0100

    fix: register Spatie role middleware aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
         $middleware->append(\App\Http\Middleware\SetLocale::class);
         $middleware->append(\App\Http\Middleware\TokenFromCookie::class);
     })
